7093da667840510d0a225b98283f22bab04404bd
Melanie Nguyen Don <mnguyendon@gmail.com>
2024-02-24 22:20:15 -0800

    mapped through past dates

--- src/components/PastDates/PastDates.jsx
+++ src/components/PastDates/PastDates.jsx
@@ -1,32 +1,38 @@
+import { useEffect, useState } from "react";
 import "./PastDates.scss";
 import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 
 function PastDates() {
+  const [currentPeople, setCurrentPeople] = useState([]);
+
+  const apiUrl = "http://localhost:5050/past-dates";
+
+  const personData = async () => {
+    try {
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+      setCurrentPeople(data);
+    } catch (error) {
+      console.error("error fetching person details", error);
+    }
+  };
+
+  useEffect(() => {
+    personData();
+  }, []);
+
   return (
     <section className="past-dates">
       <div className="past-dates__container">
         <h2 className="past-dates__title">Past Dates</h2>
-
-        <Link to="/past-dates/:id" className="person__link">
-          <div className="person person-specific">
-            <p className="person__category">Daniel</p>
-          </div>
-        </Link>
-        <Link to="/past-dates/:id" className="person__link">
-          <div className="person person-specific">
-            <p className="person__category">Daniel</p>
-          </div>
-        </Link>
-        <Link to="/past-dates/:id" className="person__link">
-          <div className="person person-specific">
-            <p className="person__category">Daniel</p>
-          </div>
-        </Link>
-        <Link to="/past-dates/:id" className="person__link">
-          <div className="person person-specific">
-            <p className="person__category">Daniel</p>
-          </div>
-        </Link>
+        {currentPeople.map((person) => (
+          <Link to={`/past-dates/${person.id}`} className="person__link">
+            <div className="person person-specific">
+              <p className="person__category">{person.name}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </section>
   );
